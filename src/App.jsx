bcff3d09e046ff1bb7402d9b80b5f3c6661a302f
AbhiKanekar <mrunmaykanekar@gmail.com>
2025-08-20 18:48:01 +0530

    Base code is done...

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-black text-white h-screen flex items-center justify-center">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
